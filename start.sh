d8e0158b5831707763732a1a22add2873168233e
Luis Vallejo <luis.vallejo@auna.org>
2025-11-28 19:20:50 -0500

    Add nginx configuration

--- start.sh
+++ start.sh
@@ -11,8 +11,14 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
+echo "Testing Nginx config..."
+nginx -t
+
 echo "Starting PHP-FPM..."
 php-fpm &
+
+echo "Waiting for PHP-FPM..."
+sleep 2
 
 echo "Starting Nginx..."
 nginx -g 'daemon off;'
